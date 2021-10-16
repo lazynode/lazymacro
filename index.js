e36d6060e29867db39024851ebf0bd387d3c7428
vang1ong7ang <vang1ong7ang@outlook.com>
2021-10-16 07:15:02 +0000

    0.1.0

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 Object.defineProperties(Object.prototype, {
-    LET: { value: function (f) { return f(this) } },
-    LETME: { value: function (f) { return f.apply(this) } },
-    APPLY: { value: function (f) { f.apply(this); return this } },
-    APPLYME: { value: function (f) { f(this); return this } },
+    PIPE: { value: function (f) { return f(this) } },
+    WITH: { value: function (f) { f(this); return this } },
+
+    PIPETHIS: { value: function (f) { return f.apply(this) } },
+    WITHTHIS: { value: function (f) { f.apply(this); return this } },
 })
