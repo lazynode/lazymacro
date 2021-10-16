5cf079cd92f8cf3527539efef1e4f574bc323c7c
vang1ong7ang <vang1ong7ang@outlook.com>
2021-10-16 08:15:02 +0000

    0.1.1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 Object.defineProperties(Object.prototype, {
     PIPE: { value: function (f) { return f(this) } },
     WITH: { value: function (f) { f(this); return this } },
+    THEN: { value: function (f) { return this.then(f) } },
 
     PIPETHIS: { value: function (f) { return f.apply(this) } },
     WITHTHIS: { value: function (f) { f.apply(this); return this } },
+    THENTHIS: { value: function (f) { return this.then(v => f.apply(v)) } },
 })
